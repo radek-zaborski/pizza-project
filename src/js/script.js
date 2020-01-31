0834ed162771e36320bcf2cb8db4f4c8cca47f78
Radoslaw Zaborski <zaborski.radoslaw@gmail.com>
2020-01-31 02:08:05 +0100

    render product

--- src/js/script.js
+++ src/js/script.js
@@ -52,16 +52,55 @@
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
   };
 
+
+  class Product {
+    constructor(id, data) {
+      const thisProduct = this;
+      thisProduct.id = id;
+      thisProduct.data = data;
+      thisProduct.renderInMenu();
+      console.log('newProduct: ', thisProduct);
+    }
+    renderInMenu(){
+      const thisProduct = this;
+      const generatedHTML = templates.menuProduct(thisProduct.data);
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+      const menuContainer = document.querySelector(select.containerOf.menu);
+      menuContainer.appendChild(thisProduct.element);
+    }
+  }
+  
   const app = {
-    init: function(){
+    
+    initMenu: function(){
       const thisApp = this;
+      console.log('thisApp.data: ', thisApp.data);
+      
+      for (let productData in thisApp.data.products){
+        new Product(productData, thisApp.data.products[productData]);
+      }
+    },
+
+    initData: function() {
+      const thisApp = this;
+
+      thisApp.data = dataSource;
+    },
+
+    init: function() {
+      const thisApp = this;   
+    
       console.log('*** App starting ***');
       console.log('thisApp:', thisApp);
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
+
+      thisApp.initData();
+      thisApp.initMenu();
     },
   };
+
 
   app.init();
 }
